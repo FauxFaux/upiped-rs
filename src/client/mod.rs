b3421eba56d2ad6d948f1225096aea378aa67e43
Chris West (Faux) <git@goeswhere.com>
2018-07-07 18:18:14 +0100

    buffering echo server?

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,4 +1,7 @@
+use std::collections::VecDeque;
+use std::io;
 use std::io::Read;
+use std::io::Write;
 
 use failure::Error;
 use mio;
@@ -6,9 +9,22 @@ use mio::net::TcpListener;
 use mio::net::TcpStream;
 use mio::Token;
 
+struct Incoming {
+    tcp: TcpStream,
+    buf: VecDeque<VecDeque<u8>>,
+}
+
+macro_rules! continue_if_blocking {
+    ($ex:expr) => {
+        match $ex {
+            Err(ref e) if io::ErrorKind::WouldBlock == e.kind() => continue,
+            other => other,
+        }
+    };
+}
+
 pub fn serve() -> Result<(), Error> {
     const SERVER: Token = Token(0);
-    const CLIENT: Token = Token(1);
 
     let addr = "127.0.0.1:13265".parse()?;
     let server = TcpListener::bind(&addr)?;
@@ -27,29 +43,71 @@ pub fn serve() -> Result<(), Error> {
     loop {
         poll.poll(&mut events, None)?;
 
-        for event in events.iter() {
+        'events: for event in events.iter() {
             match event.token() {
                 SERVER => {
-                    let (stream, addr) = server.accept()?;
-                    poll.register(
-                        &stream,
-                        Token(2),
-                        mio::Ready::readable(),
-                        mio::PollOpt::edge(),
-                    )?;
-                    streams.push(stream);
-                    println!("reg");
+                    let (stream, addr) = continue_if_blocking!(server.accept())?;
+                    let token = Token(streams.len() + 1);
+                    poll.register(&stream, token, mio::Ready::all(), mio::PollOpt::edge())?;
+                    streams.push(Incoming {
+                        tcp: stream,
+                        buf: VecDeque::new(),
+                    });
                 }
-                Token(2) => {
-                    let mut stream = streams.pop().unwrap();
-                    let mut buf = [0u8; 16];
-                    stream.read(&mut buf)?;
-                    println!("{:?}", String::from_utf8_lossy(&buf));
+                Token(other) => {
+                    let token = other - 1;
+                    let incom = &mut streams[token];
+
+                    if event.readiness().is_readable() {
+                        let mut found = VecDeque::new();
+                        let mut buf = [0u8; 16];
+                        loop {
+                            match incom.tcp.read(&mut buf).map_non_block()? {
+                                Some(bytes) => found.extend(&buf[..bytes]),
+                                None => break,
+                            }
+                        }
+                        incom.buf.push_back(found);
+                    }
+
+                    if event.readiness().is_writable() {
+                        while !incom.buf.is_empty() {
+                            {
+                                let reading = &mut incom.buf[0];
+                                while let Some(byte) = reading.pop_front() {
+                                    if incom.tcp.write(&[byte]).map_non_block()?.is_none() {
+                                        continue 'events;
+                                    }
+                                }
+                            }
+                            incom.buf.pop_front();
+                        }
+                    }
                 }
-                _ => unreachable!(),
             }
         }
     }
 
     Ok(())
+}
+
+trait MapNonBlock<T> {
+    fn map_non_block(self) -> Result<Option<T>, io::Error>;
+}
+
+impl<T> MapNonBlock<T> for Result<T, io::Error> {
+    fn map_non_block(self) -> Result<Option<T>, io::Error> {
+        use std::io::ErrorKind::WouldBlock;
+
+        match self {
+            Ok(value) => Ok(Some(value)),
+            Err(err) => {
+                if let WouldBlock = err.kind() {
+                    Ok(None)
+                } else {
+                    Err(err)
+                }
+            }
+        }
+    }
 }
