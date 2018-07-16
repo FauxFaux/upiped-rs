258ca2316e7eb98b7a902a59e46514b3c2814011
Chris West (Faux) <git@goeswhere.com>
2018-07-16 20:06:27 +0100

    gracefully? handle client closing connection

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::io;
 use std::io::Read;
@@ -38,7 +39,8 @@ pub fn serve() -> Result<(), Error> {
 
     let mut events = mio::Events::with_capacity(1024);
 
-    let mut streams = Vec::new();
+    let mut streams = HashMap::new();
+    let mut next_token = 1;
 
     loop {
         poll.poll(&mut events, None)?;
@@ -47,29 +49,43 @@ pub fn serve() -> Result<(), Error> {
             match event.token() {
                 SERVER => {
                     let (stream, addr) = continue_if_blocking!(server.accept())?;
-                    let token = Token(streams.len() + 1);
+                    let token = Token(next_token);
                     poll.register(&stream, token, mio::Ready::all(), mio::PollOpt::edge())?;
-                    streams.push(Incoming {
-                        tcp: stream,
-                        buf: VecDeque::new(),
-                    });
+                    streams.insert(
+                        next_token,
+                        Incoming {
+                            tcp: stream,
+                            buf: VecDeque::new(),
+                        },
+                    );
+                    next_token += 1;
                 }
-                Token(other) => {
-                    let token = other - 1;
-                    let incom = &mut streams[token];
+                Token(token) => {
+                    let mut eof = false;
+                    {
+                        let incom = streams
+                            .get_mut(&token)
+                            .ok_or_else(|| format_err!("wakeup for invalid token"))?;
 
-                    if event.readiness().is_readable() {
-                        let found = read_until_blocks(&incom.tcp)?;
-                        if !found.is_empty() {
-                            incom.buf.push_back(found);
+                        if event.readiness().is_readable() {
+                            match read_until_blocks(&incom.tcp)? {
+                                Some(ref buf) if buf.is_empty() => (),
+                                Some(found) => incom.buf.push_back(found),
+                                None => {
+                                    eof = true;
+                                }
+                            }
+                        }
+
+                        if event.readiness().is_writable() {
+                            while !incom.buf.is_empty() {
+                                drain_some_writeable(&mut incom.buf[0], &incom.tcp)?;
+                                incom.buf.pop_front();
+                            }
                         }
                     }
-
-                    if event.readiness().is_writable() {
-                        while !incom.buf.is_empty() {
-                            drain_some_writeable(&mut incom.buf[0], &incom.tcp)?;
-                            incom.buf.pop_front();
-                        }
+                    if eof {
+                        streams.remove(&token);
                     }
                 }
             }
@@ -79,13 +95,17 @@ pub fn serve() -> Result<(), Error> {
     Ok(())
 }
 
-fn read_until_blocks<R: Read>(mut from: R) -> Result<VecDeque<u8>, Error> {
+/// `None` on EOF
+fn read_until_blocks<R: Read>(mut from: R) -> Result<Option<VecDeque<u8>>, Error> {
     let mut found = VecDeque::new();
     let mut buf = [0u8; 256];
     while let Some(bytes) = from.read(&mut buf).map_non_block()? {
+        if 0 == bytes {
+            return Ok(None);
+        }
         found.extend(&buf[..bytes]);
     }
-    Ok(found)
+    Ok(Some(found))
 }
 
 fn drain_some_writeable<W: Write>(reading: &mut VecDeque<u8>, mut into: W) -> Result<(), Error> {
